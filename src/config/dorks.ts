4d78be0ea713cf33dd9f5d2f675f9e40f5ce307a
hoanglong <87950841+socialist-dev@users.noreply.github.com>
2026-09-10 19:11:24 +0700

    Create dorks.ts

---
+++ src/config/dorks.ts
@@ -0,0 +1,12 @@
+export const DORK_QUERIES = [
+  // Threads - Video Editing & Design
+  'site:threads.net ("tuyển editor" OR "tìm editor" OR "cần edit video" OR "edit capcut" OR "edit tiktok")',
+  'site:threads.net ("cần designer" OR "tìm thiết kế" OR "tuyển 2d designer" OR "tìm người làm thumbnail")',
+  
+  // Facebook Groups / Public Posts
+  'site:facebook.com/groups ("cần tìm editor gấp" OR "tìm người edit video" OR "cần edit reels")',
+  'site:facebook.com/groups ("cần làm banner" OR "tìm designer freelance" OR "job edit video")',
+  
+  // X (Twitter)
+  'site:x.com ("tuyển editor" OR "cần tìm editor" OR "tìm designer freelance")'
+];
